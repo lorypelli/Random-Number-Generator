779e110bd65a525f7bb2c71b2c458d3d8f5229ea
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-11 14:46:36 +0100

    use vite port

--- astro.config.ts
+++ astro.config.ts
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
-
-// https://astro.build/config
 export default defineConfig({
     output: 'server',
-    adapter: vercel()
+    adapter: vercel(),
+    server: {
+        host: '127.0.0.1',
+        port: 5173
+    }
 });
