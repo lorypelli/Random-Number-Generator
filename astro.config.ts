234786abf4a10a8e3f6415712eff8904aec1b30f
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-11 21:25:47 +0100

    generate css file

--- astro.config.ts
+++ astro.config.ts
@@ -6,5 +6,10 @@ export default defineConfig({
     server: {
         host: '127.0.0.1',
         port: 5173
+    },
+    vite: {
+        build: {
+            assetsInlineLimit: 0
+        }
     }
 });
