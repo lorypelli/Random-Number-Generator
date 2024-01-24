d1a6a9d55d70f791a36735e7e1e91ac76cfadd8e
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-24 21:28:57 +0100

    implemented a potential fix for production

--- src/utils/isValidLang.ts
+++ src/utils/isValidLang.ts
@@ -1,7 +1,8 @@
 import fs from 'fs';
 export default function isValidLang(l: string) {
-    const directory = './src/translations/';
-    const files = fs.readdirSync(directory);
+    const devDirectory = './public/translations/';
+    const prodDirectory = './translations/';
+    const files = fs.existsSync(prodDirectory) ? fs.readdirSync(prodDirectory) : fs.readdirSync(devDirectory);
     for (let i = 0; i < files.length; i++) {
         if (l == files[i].split('.')[0]) {
             return true;
