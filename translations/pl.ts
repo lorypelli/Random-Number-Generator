c0db9ad4cc7415ecda26734f02106dd86777ef6e
RVG|lory <87276663+lorypelli@users.noreply.github.com>
2024-01-25 21:33:20 +0100

    New translations en.ts (Polish)

---
+++ translations/pl.ts
@@ -0,0 +1,50 @@
+export const translations = {
+  document: {
+    title: 'Random Number Generator',
+    meta: {
+      description: 'Generate random numbers',
+      og: {
+        title: 'Random Number Generator',
+        description: 'Generate random numbers'
+      }
+    }
+  },
+  theme: {
+    change: 'Change Theme'
+  },
+  error: {
+    generic: 'There was an error'
+  },
+  numbers: {
+    amount: 'Numbers Amount'
+  },
+  mode: {
+    decimal: 'Decimal',
+    binary: 'Binary',
+    octal: 'Octal',
+    hex: 'Hexadecimal'
+  },
+  frequency: {
+    repeat: 'Repeat',
+    no_repeat: 'No Repeat'
+  },
+  number: {
+    min: 'Min Number',
+    max: 'Max Number'
+  },
+  gbutton: {
+    text: 'Generate'
+  },
+  generate: {
+    label: {
+      single: 'The random number in base {mode} between {hex}{minNumber} and {hex}{maxNumber} is:',
+      multiple: 'The {numbers} random numbers in base {mode} between {hex}{minNumber} and {hex}{maxNumber} are:',
+      duplicate: 'Some numbers weren\'t generated because they were a duplicate',
+      previous: 'Previous Numbers'
+    },
+    buttons: {
+      another: 'Generate Another',
+      intervals: 'Change Intervals'
+    }
+  }
+};
