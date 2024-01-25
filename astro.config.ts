806dfd3ad73ac1202629d920c3caff55f5e10c96
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-25 21:39:41 +0100

    fetch from github

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,7 @@ import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel/serverless';
 export default defineConfig({
     output: 'server',
-    adapter: vercel({
-        includeFiles: ['./translations/en.ts', './translations/it.ts']
-    }),
+    adapter: vercel(),
     server: {
         open: true,
         host: '127.0.0.1',
--- src/utils/isValidLang.ts
+++ src/utils/isValidLang.ts
@@ -1,9 +1,23 @@
-import fs from 'fs';
-export default function isValidLang(l: string) {
-    const directory = './translations';
-    const files = fs.readdirSync(directory);
+interface GithubFiles {
+    name: string,
+    path: string,
+    sha: string,
+    size: number,
+    url: string,
+    html_url: string,
+    git_url: string,
+    download_url: string,
+    type: string,
+    _links: {
+        self: string,
+        git: string,
+        html: string
+    }
+}
+export default async function isValidLang(l: string) {
+    const files: GithubFiles[] = await fetch('https://api.github.com/repos/lorypelli/Random-Number-Generator/contents/translations').then(async f => await f.json());
     for (let i = 0; i < files.length; i++) {
-        if (l == files[i].split('.')[0]) {
+        if (l == files[i].name.split('.')[0]) {
             return true;
         }
     }
