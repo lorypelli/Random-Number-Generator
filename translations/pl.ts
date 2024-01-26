be08d4c5c44d1e703975bcb97e286bf8a61a5ddf
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-26 21:21:03 +0100

    removed github integration and fixed indentation (I was really hating it)

--- translations/pl.ts
+++ translations/pl.ts
@@ -1,50 +1,50 @@
 export const translations = {
-  document: {
-    title: "Generator Losowych Liczb",
-    meta: {
-      description: "Generuj losowe liczby",
-      og: {
-        title: "Generator Losowych Liczb",
-        description: "Generuj losowe liczby"
-      }
-    }
-  },
-  theme: {
-    change: "Zmień Motyw"
-  },
-  error: {
-    generic: "Wystąpił błąd"
-  },
-  numbers: {
-    amount: "Ilość liczb"
-  },
-  mode: {
-    decimal: "Dziesiętny",
-    binary: "Dwójkowy",
-    octal: "Ósemkowy",
-    hex: "Szesnastkowy"
-  },
-  frequency: {
-    repeat: "Powtórz",
-    no_repeat: "Nie powtarzaj"
-  },
-  number: {
-    min: "Min Liczba",
-    max: "Max Liczba"
-  },
-  gbutton: {
-    text: "Generuj"
-  },
-  generate: {
-    label: {
-      single: "Losowa liczba {mode} pomiędzy {hex}{minNumber} a {hex}{maxNumber} to:",
-      multiple: "Liczby losowe {numbers} w bazie {mode} między {hex}{minNumber} i {hex}{maxNumber} to:",
-      duplicate: "Niektóre liczby nie zostały wygenerowane, ponieważ były duplikatem",
-      previous: "Poprzednie cyfry"
+    document: {
+        title: 'Generator Losowych Liczb',
+        meta: {
+            description: 'Generuj losowe liczby',
+            og: {
+                title: 'Generator Losowych Liczb',
+                description: 'Generuj losowe liczby'
+            }
+        }
     },
-    buttons: {
-      another: "Generuj kolejne",
-      intervals: "Zmień przedziały"
+    theme: {
+        change: 'Zmień Motyw'
+    },
+    error: {
+        generic: 'Wystąpił błąd'
+    },
+    numbers: {
+        amount: 'Ilość liczb'
+    },
+    mode: {
+        decimal: 'Dziesiętny',
+        binary: 'Dwójkowy',
+        octal: 'Ósemkowy',
+        hex: 'Szesnastkowy'
+    },
+    frequency: {
+        repeat: 'Powtórz',
+        no_repeat: 'Nie powtarzaj'
+    },
+    number: {
+        min: 'Min Liczba',
+        max: 'Max Liczba'
+    },
+    gbutton: {
+        text: 'Generuj'
+    },
+    generate: {
+        label: {
+            single: 'Losowa liczba {mode} pomiędzy {hex}{minNumber} a {hex}{maxNumber} to:',
+            multiple: 'Liczby losowe {numbers} w bazie {mode} między {hex}{minNumber} i {hex}{maxNumber} to:',
+            duplicate: 'Niektóre liczby nie zostały wygenerowane, ponieważ były duplikatem',
+            previous: 'Poprzednie cyfry'
+        },
+        buttons: {
+            another: 'Generuj kolejne',
+            intervals: 'Zmień przedziały'
+        }
     }
-  }
 };
