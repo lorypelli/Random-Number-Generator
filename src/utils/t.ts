9bafc22956088e6ffb06cc522ad45b7ae5925975
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-26 07:07:20 +0100

    added Polish language

--- src/utils/t.ts
+++ src/utils/t.ts
@@ -1,5 +1,6 @@
 import { translations as en } from '../../translations/en.ts';
 import { translations as it } from '../../translations/it.ts';
+import { translations as pl } from '../../translations/pl.ts';
 export default function t(l: string, s: string, ...a: string[]) {
     const arr = s.split('.');
     switch (l) {
@@ -34,6 +35,28 @@ export default function t(l: string, s: string, ...a: string[]) {
             }
             return {};
         }, it);
+        if (typeof v == 'string') {
+            if (v.includes('{')) {
+                let str = v;
+                let c = 0;
+                str = v.replace(/\{.*?\}/g, () => {
+                    const r = a[c];
+                    c++;
+                    return r;
+                });
+                return str;
+            }
+            return v;
+        }
+        return '';
+    }
+    case 'pl': {
+        const v = arr.reduce<string | object>((o, k) => {
+            if (typeof o == 'object' && k in o) {
+                return o[k as keyof typeof o];
+            }
+            return {};
+        }, pl);
         if (typeof v == 'string') {
             if (v.includes('{')) {
                 let str = v;
