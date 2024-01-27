385720b29e8a4914015e0a4d12a225410ba7f1d6
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-27 07:12:02 +0100

    Added Maori Language

--- src/utils/t.ts
+++ src/utils/t.ts
@@ -1,6 +1,7 @@
 import { translations as en } from '../../translations/en.ts';
 import { translations as it } from '../../translations/it.ts';
 import { translations as pl } from '../../translations/pl.ts';
+import { translations as mi } from '../../translations/mi.ts';
 export default function t(l: string, s: string, ...a: string[]) {
     const arr = s.split('.');
     switch (l) {
@@ -57,6 +58,28 @@ export default function t(l: string, s: string, ...a: string[]) {
             }
             return {};
         }, pl);
+        if (typeof v == 'string') {
+            if (v.includes('{')) {
+                let str = v;
+                let c = 0;
+                str = v.replace(/\{.*?\}/g, () => {
+                    const r = a[c];
+                    c++;
+                    return r;
+                });
+                return str;
+            }
+            return v;
+        }
+        return '';
+    }
+    case 'mi': {
+        const v = arr.reduce<string | object>((o, k) => {
+            if (typeof o == 'object' && k in o) {
+                return o[k as keyof typeof o];
+            }
+            return {};
+        }, mi);
         if (typeof v == 'string') {
             if (v.includes('{')) {
                 let str = v;
---
+++ translations/mi.ts
@@ -0,0 +1,50 @@
+export const translations = {
+    document: {
+        title: 'Whakahiko Tau Matapōkere',
+        meta: {
+            description: 'Whakaputa tau matapōkere',
+            og: {
+                title: 'Whakahiko Tau Matapōkere',
+                description: 'Whakaputa tau matapōkere'
+            }
+        }
+    },
+    theme: {
+        change: 'Whakarerekē Āhua'
+    },
+    error: {
+        generic: 'I puta mai he hapa'
+    },
+    numbers: {
+        amount: 'Rahinga Tau'
+    },
+    mode: {
+        decimal: 'Pūnaha Tekau',
+        binary: 'Pūnaha Rua',
+        octal: 'Pūnaha Waru',
+        hex: 'Pūnaha Tekau-ono'
+    },
+    frequency: {
+        repeat: 'Tukurua',
+        no_repeat: 'Kore Tukurua'
+    },
+    number: {
+        min: 'Tau Mōkito',
+        max: 'Tau Mōrahi'
+    },
+    gbutton: {
+        text: 'Whakaputa'
+    },
+    generate: {
+        label: {
+            single: 'Ko te tau matapōkere i roto i te {mode} i warnganui i {hex}{minNumber} me {hex}{maxNumber} ko:',
+            multiple: 'Ko ngā tau matapōkere {numbers} i roto i te {mode} i warnganui i {hex}{minNumber} me {hex}{maxNumber} ko:',
+            duplicate: 'Kāore i hangaia ētahi tau i te mea he tāruarua',
+            previous: 'Ngā Tau Kua Pahure'
+        },
+        buttons: {
+            another: 'Whakaputa Atu Anō',
+            intervals: 'Panoni Ngā Whiringa'
+        }
+    }
+};
