d01e88ba7624fcd1e4fd644d2d368cf66f268132
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-27 18:52:30 +0100

    fallback to english instead of redirecting

--- src/utils/isValidLang.ts
+++ src/utils/isValidLang.ts
@@ -15,7 +15,9 @@ interface GithubFiles {
     }
 }
 export default async function isValidLang(l: string) {
-    const files: GithubFiles[] = await fetch('https://api.github.com/repos/lorypelli/Random-Number-Generator/contents/translations').then(async f => await f.json());
+    const files: GithubFiles[] = await fetch('https://api.github.com/repos/lorypelli/Random-Number-Generator/contents/translations').then(async (f) => await f.json()).catch(() => {
+        return false;
+    });
     for (let i = 0; i < files.length; i++) {
         if (l == files[i].name.split('.')[0]) {
             return true;
