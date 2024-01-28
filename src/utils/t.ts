e78c27a8f30aea1bff709dd017e91b905a8f117e
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-28 08:59:25 +0100

    fix double check

--- src/utils/t.ts
+++ src/utils/t.ts
@@ -14,16 +14,14 @@ export default function t(l: string, s: string, ...a: string[]) {
         }, en);
         if (typeof v == 'string') {
             if (v.includes('{')) {
-                if (v.includes('{')) {
-                    let str = v;
-                    let c = 0;
-                    str = v.replace(/\{.*?\}/g, () => {
-                        const r = a[c];
-                        c++;
-                        return r;
-                    });
-                    return str;
-                }
+                let str = v;
+                let c = 0;
+                str = v.replace(/\{.*?\}/g, () => {
+                    const r = a[c];
+                    c++;
+                    return r;
+                });
+                return str;
             }
             return v;
         }
