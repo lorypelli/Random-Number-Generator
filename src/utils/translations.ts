c1f67c3d54cf0059bf7952b6a5811e7877deea1a
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-01-30 15:36:12 +0100

    only valid Translations string keys

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -3,7 +3,58 @@ import { translations as it } from '../../translations/it.ts';
 import { translations as pl } from '../../translations/pl.ts';
 import { translations as mi } from '../../translations/mi.ts';
 import { readdirSync } from 'fs';
-export function t(l: string, s: string, ...a: string[]) {
+interface Translations {
+    document: {
+        title: string;
+        meta: {
+            description: string;
+            og: {
+                title: string;
+                description: string;
+            };
+        };
+    };
+    theme: {
+        change: string;
+    };
+    error: {
+        generic: string;
+    };
+    numbers: {
+        amount: string;
+    };
+    mode: {
+        decimal: string;
+        binary: string;
+        octal: string;
+        hex: string;
+    };
+    frequency: {
+        repeat: string;
+        no_repeat: string;
+    };
+    number: {
+        min: string;
+        max: string;
+    };
+    gbutton: {
+        text: string;
+    };
+    generate: {
+        label: {
+            single: string;
+            multiple: string;
+            duplicate: string;
+            previous: string;
+        };
+        buttons: {
+            another: string;
+            intervals: string;
+        };
+    };
+}
+type SKey<T> = { [K in Extract<keyof T, string>]: T[K] extends string ? K : T[K] extends object ? `${K}.${SKey<T[K]>}` : never}[Extract<keyof T, string>]
+export function t(l: string, s: SKey<Translations>, ...a: string[]) {
     const arr = s.split('.');
     switch (l) {
     case 'en': {
