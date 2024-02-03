08aa394022be5bd09c0cd9addefed5a1ec07d38a
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-02-03 14:26:34 +0100

    fix `this` invalid context

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -3,4 +3,4 @@ import { translations as en } from '../../translations/en';
 import { translations as it } from '../../translations/it';
 import { translations as mi } from '../../translations/mi';
 import { translations as pl } from '../../translations/pl';
-export const { t, isValidLang } = new Translation(['en', 'it', 'mi', 'pl'], en, it, mi, pl);
+export const translate = new Translation(['en', 'it', 'mi', 'pl'], en, it, mi, pl);
