f615084d4598b6b861590f14e284450e0cf3d437
LoryPelli <87276663+LoryPelli@users.noreply.github.com>
2024-03-03 17:51:09 +0100

    renamed class

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -1,6 +1,6 @@
-import { Translation } from 'trslate';
+import { TContext } from 'trslate';
 import { translations as en } from '../../translations/en';
 import { translations as it } from '../../translations/it';
 import { translations as mi } from '../../translations/mi';
 import { translations as pl } from '../../translations/pl';
-export const translate = new Translation(['en', 'it', 'mi', 'pl'], en, it, mi, pl);
+export const translate = new TContext(['en', 'it', 'mi', 'pl'], en, it, mi, pl);
